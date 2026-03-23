90353532847a240d66209d98c4de9127ad8f499a
Han Pate <bujo.hannah@gmail.com>
2026-03-23 12:41:08 -0600

    final

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-from typing import List, Tuple
+'''
+Han Pate
+CSCI 332 Spring 2026
+Programming Assignment | Convex Hull Jarvis
+I acknowledge that I have worked on this assignment independently, except
+where explicitly noted and referenced. Any collaboration or use of external
+resources has been properly cited. I am fully aware of the consequences of
+academic dishonesty and agree to abide by the university's academic integrity
+policy. I understand the importance the consequences of plagiarism.
+'''
 
-Point = Tuple[float, float]
+Point = tuple[float,float] #Point is capitalized bc its a type not a variable
+
+def orientation(p: Point,q: Point,r: Point): #this function uses the convex hull formula & calculates it
+    val = (q[0] - p[0]) * (r[1] - p[1]) - (q[1] - p[1]) * (r[0] - p[0]) #the formula for convex hull
+    return val #return the value
 
 def convex_hull_jarvis(points: List[Point]) -> List[Point]:
-   pass
+    if len(points) < 3: #if there are less than 3 pts
+        return [] #return an empty list
+    hull = [] # a list to store the points
+    start = min(points, key=lambda p: (p[0], p[1])) #find the leftmost point, look at formula
+    current = start
+
+    while True:
+        hull.append(current)
+        next_point = points[0] if points[0] != current else points[1]
+
+        for candidate in points:
+            if orientation(current, next_point, candidate) > 0: #pts will be > 0 if they're counterclockwise
+                next_point = candidate
+
+        current = next_point
+        if current == start: #if the current point is now the start
+            break #breaks out of the loop
+    return hull
 
 
-if __name__ == "__main__":
-    points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)] 
-    hull = convex_hull_jarvis(points) 
-    print("Convex Hull:", hull)
+
+points = [(0,3), (2,2), (1,1), (2,1), (3,0), (0,0), (3,3)]
+hull = convex_hull_jarvis(points)
+print('Convex hull:', hull)
+#Should print: Convex Hull: [(0, 0), (0, 3), (3, 3), (3, 0)]
+#output should be a list of pts forming the convex in counter clockwise orderfrom typing import List, Tuple
 
 
 #Convex Hull: [(0, 0), (0, 3), (3, 3), (3, 0)]
--- tests.py
+++ tests.py
@@ -1,32 +1,34 @@
+'''
+Han Pate
+CSCI 332 Spring 2026
+Programming Assignment | Convex Hull Jarvis
+I acknowledge that I have worked on this assignment independently, except
+where explicitly noted and referenced. Any collaboration or use of external
+resources has been properly cited. I am fully aware of the consequences of
+academic dishonesty and agree to abide by the university's academic integrity
+policy. I understand the importance the consequences of plagiarism.
+'''
+
 import unittest
 from main import convex_hull_jarvis
 
-
-class TestMathFunctions(unittest.TestCase):
-    
-    def test_example_from_prompt(self):
-        exit(1)
-
-    def test_square_with_inner_points(self):
-        exit(1)
-
-    def test_duplicates(self):
-        exit(1)
-
-    def test_all_collinear(self):
-        exit(1)
-
-    def test_two_points(self):
-        exit(1)
-
-    def test_single_point(self):
-       exit(1)
-
+class TestConvexHull(unittest.TestCase):
+    def test_class_example(self): #testing the class example
+        points = [(0,3), (2,2), (1,1), (2,1), (3,0), (0,0), (3,3)]
+        result = convex_hull_jarvis(points)
+        self.assertEqual(result, [(0, 0), (0, 3), (3, 3), (3, 0)])
     def test_triangle(self):
-        exit(1)
+        points = [(0, 0), (1, 2), (2, 0)]
+        result = convex_hull_jarvis(points)
+        self.assertEqual(result, [(0, 0), (1, 2), (2, 0)])
+    def test_interior_points(self):
+        points = [(0, 0), (0, 3), (3, 3), (3, 0), (1, 1)]
+        result = convex_hull_jarvis(points)
+        self.assertEqual(result,[(0, 0), (0, 3), (3, 3), (3, 0)])
+    def test_not_enough(self):
+        points = [(0, 0), (1, 1)]
+        result = convex_hull_jarvis(points)
+        self.assertEqual(result, [])
 
-    def test_collinear_on_edges(self):
-        exit(1)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
